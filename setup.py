932e3c88e776474911cfe330b98acdc216f6e179
elbuo8 <yamil.asusta@sendgrid.com>
2014-01-29 14:59:53 -0500

    bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='smtpapi',
-    version='0.1.0',
+    version='0.1.1',
     author='Yamil Asusta, Kane Kim',
     author_email='yamil@sendgrid.com, kane.isturm@sendgrid.com',
     packages=find_packages(),
