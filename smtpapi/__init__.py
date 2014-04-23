3f5cd8e330b4f8a153dd7453b1dfc2bd21d5e034
elbuo8 <yamil.asusta@sendgrid.com>
2014-04-23 10:38:08 -0600

    PEP8 all the things! Plus removed empty values when building the header

--- smtpapi/__init__.py
+++ smtpapi/__init__.py
@@ -1,60 +1,64 @@
 import json
 
+
 class SMTPAPIHeader(object):
 
-  def __init__(self):
-    self.data = {}
+    def __init__(self):
+        self.data = {}
 
-  def add_to(self, to):
-    if 'to' not in self.data:
-      self.data['to'] = []
-    self.data['to'].append(to)
+    def add_to(self, to):
+        if 'to' not in self.data:
+            self.data['to'] = []
+        self.data['to'].append(to)
 
-  def set_tos(self, tos):
-    self.data['to'] = tos
+    def set_tos(self, tos):
+        self.data['to'] = tos
 
-  def add_substitution(self, key, value):
-    if 'sub' not in self.data:
-      self.data['sub'] = {}
-    if key not in self.data['sub']:
-      self.data['sub'][key] = []
-    self.data['sub'][key].append(value)
+    def add_substitution(self, key, value):
+        if 'sub' not in self.data:
+            self.data['sub'] = {}
+        if key not in self.data['sub']:
+            self.data['sub'][key] = []
+        self.data['sub'][key].append(value)
 
-  def set_substitutions(self, subs):
-    self.data['sub'] = subs
+    def set_substitutions(self, subs):
+        self.data['sub'] = subs
 
-  def add_unique_arg(self, key, value):
-    if 'unique_args' not in self.data:
-      self.data['unique_args'] = {}
-    self.data['unique_args'][key] = value
+    def add_unique_arg(self, key, value):
+        if 'unique_args' not in self.data:
+            self.data['unique_args'] = {}
+        self.data['unique_args'][key] = value
 
-  def set_unique_args(self, value):
-    self.data['unique_args'] = value
+    def set_unique_args(self, value):
+        self.data['unique_args'] = value
 
-  def add_category(self, category):
-    if 'category' not in self.data:
-      self.data['category'] = []
-    self.data['category'].append(category)
+    def add_category(self, category):
+        if 'category' not in self.data:
+            self.data['category'] = []
+        self.data['category'].append(category)
 
-  def set_categories(self, category):
-    self.data['category'] = category
+    def set_categories(self, category):
+        self.data['category'] = category
 
-  def add_section(self, key, section):
-    if 'section' not in self.data:
-      self.data['section'] = {}
-    self.data['section'][key] = section
+    def add_section(self, key, section):
+        if 'section' not in self.data:
+            self.data['section'] = {}
+        self.data['section'][key] = section
 
-  def set_sections(self, value):
-    self.data['section'] = value
+    def set_sections(self, value):
+        self.data['section'] = value
 
-  def add_filter(self, app, setting, val):
-    if 'filters' not in self.data:
-      self.data['filters'] = {}
-    if app not in self.data['filters']:
-      self.data['filters'][app] = {}
-    if 'settings' not in self.data['filters'][app]:
-      self.data['filters'][app]['settings'] = {}
-    self.data['filters'][app]['settings'][setting] = val
+    def add_filter(self, app, setting, val):
+        if 'filters' not in self.data:
+            self.data['filters'] = {}
+        if app not in self.data['filters']:
+            self.data['filters'][app] = {}
+        if 'settings' not in self.data['filters'][app]:
+            self.data['filters'][app]['settings'] = {}
+        self.data['filters'][app]['settings'][setting] = val
 
-  def json_string(self):
-    return json.dumps(self.data)
+    def json_string(self):
+        for key in self.data.keys():
+            if self.data[key] == [] or self.data[key] == {}:
+                del self.data[key]
+        return json.dumps(self.data)
