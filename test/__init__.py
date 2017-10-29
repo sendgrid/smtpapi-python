34ebdcf7551601d97cd115cfdce46b813fcfd2df
riyasp <riyasp@sahajsoft.com>
2017-10-29 14:38:21 +0530

    added unittest to check for specific repo files

--- test/__init__.py
+++ test/__init__.py
@@ -1,5 +1,10 @@
-import unittest, json, decimal
+import decimal
+import json
+import os
+import unittest
+
 from smtpapi import SMTPAPIHeader
+
 
 class TestSMTPAPI(unittest.TestCase):
 
@@ -70,6 +75,38 @@ class TestSMTPAPI(unittest.TestCase):
         header.set_ip_pool(None)
         header.add_filter('testFilter', 'filter', 'filterValue')
         self.assertEqual(self.dropsHeader, json.loads(header.json_string()))
+
+
+class TestFilesExist(unittest.TestCase):
+    def setUp(self):
+        self.files_required = [
+            ['./Docker', 'docker/Docker'],
+            ['./docker-compose.yml', './docker/docker-compose.yml'],
+            './.env_sample',
+            './.gitignore',
+            './.travis.yml',
+            './.codeclimate.yml',
+            './CHANGELOG.md',
+            './CODE_OF_CONDUCT.md',
+            './CONTRIBUTING.md',
+            './.github/ISSUE_TEMPLATE',
+            './LICENSE.md',
+            './.github/PULL_REQUEST_TEMPLATE',
+            './README.md',
+            './TROUBLESHOOTING.md',
+            './USAGE.md',
+            './USE_CASES.md'
+        ]
+        self.file_missing_msg = '"{}" missing in repo'
+
+    def test_file_exists(self):
+
+        for file in self.files_required:
+            if isinstance(file, list):
+                self.assertTrue(any(os.path.exists(f) for f in file),
+                                msg=self.file_missing_msg.format('" or "'.join(file)))
+            else:
+                self.assertTrue(os.path.exists(file), msg=self.file_missing_msg.format(file))
 
 
 if __name__ == '__main__':
