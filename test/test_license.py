f5af067b9ed7e735a07d56e158c6dd35df5ace8e
Hugo <hugovk@users.noreply.github.com>
2018-10-20 15:54:15 +0300

    Fix unittest subclass

--- test/test_license.py
+++ test/test_license.py
@@ -6,9 +6,12 @@ except ImportError:
 import datetime
 
 
+class LicenseTests(unittest.TestCase):
+    def test_license_year(self):
+        with open("license.txt", "r") as f:
+            copyright_line = f.readline().rstrip()
 
-def test_license_year(self):
-    LICENSE_FILE = 'license.txt'
-    with open(LICENSE_FILE, 'r') as f:
-        copyright_line = f.readline().rstrip()
-    self.assertEqual('Copyright (c) 2012-%s SendGrid, Inc.' % datetime.datetime.now().year, copyright_line)
+        self.assertEqual(
+            "Copyright (c) 2012-%s SendGrid, Inc." % datetime.datetime.now().year,
+            copyright_line,
+        )
