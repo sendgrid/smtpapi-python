ac6801603991d2be233db444f2caf0bb96f040a2
Elise Shanholtz <eshanholtz@twilio.com>
2020-01-15 10:33:36 -0800

    update tests

--- test/test_lisence.py
+++ test/test_lisence.py
@@ -19,4 +19,4 @@ def test_license_year(self):
     LICENSE_FILE = 'license.txt'
     with open(LICENSE_FILE, 'r') as f:
         copyright_line = f.readline().rstrip()
-    self.assertEqual('Copyright (c) 2012-%s SendGrid, Inc.' % datetime.datetime.now().year, copyright_line)
+    self.assertEqual('Copyright (c) 2012-%s Twilio SendGrid, Inc.' % datetime.datetime.now().year, copyright_line)
