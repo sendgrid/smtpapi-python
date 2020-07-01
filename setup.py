b3e29d5b5cf918b5281dc6b8c91bbe2d3ca73209
childish-sambino <sharrison@twilio.com>
2020-07-01 12:59:16 -0500

    fix: update supported Python versions in setup file

--- setup.py
+++ setup.py
@@ -22,9 +22,11 @@ setup(
     description='Simple wrapper to use SendGrid SMTP API',
     long_description=readme,
     classifiers=[
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 )
